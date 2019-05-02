e965d5750e7c8321398ef612b457bb7df9f4df5c
ThunderDev1 <sgranger1@gmail.com>
2019-05-02 16:37:52 +0200

    Added TypeScript

---
+++ src/App.tsx
@@ -0,0 +1,6 @@
+import { hot } from "react-hot-loader/root";
+import * as React from "react";
+
+const App = () => <div>Hello TS!</div>;
+
+export default hot(App);
--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,35 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
-      },
-      {
-        test: /\.html$/,
-        use: [
-          {
-            loader: "html-loader"
-          }
-        ]
-      }
-    ]
+  mode: "development",
+  devtool: "inline-source-map",
+  entry: "./src/index.tsx",  
+  output: {
+    filename: "bundle.js"
   },
   resolve: {
+    extensions: [".ts", ".tsx", ".js"],
     alias: {
       'react-dom': '@hot-loader/react-dom'
     }
   },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader", "ts-loader"]
+      },
+      {
+        test: /\.html$/,
+        use: [ "html-loader"]
+      }
+    ]
+  },
+  
   plugins: [
     new HtmlWebPackPlugin({
-      template: "./index.html",
-      filename: "./index.html"
+      template: "./index.html"
     })
   ]
 };
