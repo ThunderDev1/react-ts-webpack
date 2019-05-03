794ade1bdd4e4cbb8872db05d01e3db96dd94b74
ThunderDev1 <sgranger1@gmail.com>
2019-05-03 08:55:04 +0200

    Added eslint and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
-import { hot } from "react-hot-loader/root";
-import * as React from "react";
+import {hot} from 'react-hot-loader/root';
+import * as React from 'react';
+import {FunctionComponent, ReactElement} from 'react';
 
-const App = () => <div>Hello TS!</div>;
+const App: FunctionComponent<{}> = (): ReactElement => {
+  return <div>Hello TS!</div>;
+};
 
 export default hot(App);
